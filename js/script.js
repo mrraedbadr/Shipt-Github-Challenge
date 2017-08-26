262e6ffd952a4c9c51013fe4373ffd5c24db9a47
Makeen Sabree <makeensabree@Macs-Device.local>
2017-08-26 19:49:34 -0400

    Fetch user info and followers working.

--- js/script.js
+++ js/script.js
@@ -1,38 +1,48 @@
-// Define a new module for our app
-var app = angular.module('instantSearch', []);
-
-// Create the instant search filter
-
-app.filter('searchFor', function(){
-
-	return function(arr, searchString){
-
-		if(!searchString){
-			return arr;
-		}
-
-		var result = [];
-
-		searchString = searchString.toLowerCase();
-
-		// Using the forEach helper method to loop through the array
-		angular.forEach(arr, function(item){
-
-			if(item.title.toLowerCase().indexOf(searchString) !== -1){
-				result.push(item);
-			}
-
-		});
-
-		return result;
+function UsernameController($scope) {
+	$scope.getGithubUserInfo = function() {
+		$scope.user = {};
+		$scope.followers = [];
+		$scope.getUserInfo().then(function(){});
 	};
 
-});
+	$scope.getUserInfo = function(){
+		return new Promise(function(resolve, reject){
+			var xhr = new XMLHttpRequest();
+			xhr.onreadystatechange = function() {
+			    if (xhr.readyState == XMLHttpRequest.DONE) {
+			    	let objUser = JSON.parse(xhr.responseText);
+			    	$scope.getFollowers(objUser.followers_url).then(function(arrFollowers){
+			    		console.log(arrFollowers)
+			    		$scope.$apply(function() {
+	  						$scope.user = objUser;
+	  						$scope.followers = arrFollowers;
+	  						console.log($scope.followers)
+						});
+			    		resolve();
+			    	})
+			    }
+			}
+			xhr.open('GET', 'https://api.github.com/users/' + $scope.username, true);
+			xhr.send(null);
+		});
+	}
 
-// The controller
+	$scope.getFollowers = function(uri){
+		return new Promise(function(resolve, reject){
+			var xhr = new XMLHttpRequest();
+			xhr.onreadystatechange = function() {
+				if (xhr.readyState == XMLHttpRequest.DONE) {
+					resolve(JSON.parse(xhr.responseText));
+				}
+			}
+			xhr.open('GET', uri, true);
+			xhr.send(null);			
+		});
+	}
 
-function InstantSearchController($scope){
+	$scope.getMoreFollowers = function(){
+		return new Promise(function(resolve, reject){
 
-	$scope.items = [];
-
+		});
+	}
 }
